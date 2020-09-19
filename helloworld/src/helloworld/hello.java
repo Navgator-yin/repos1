119eb4778cad45570e4bafa946c3e5cfd3ae2a45
Navgator_yin <1020351371@qq.com>
2020-09-19 19:51:03 +0800

    添加注释

--- helloworld/src/helloworld/hello.java
+++ helloworld/src/helloworld/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		float a=3.14f;
-		System.out.println(a);
+		System.out.println(a);//输出
 		
 	}
 
