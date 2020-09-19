63c0c488adec7511f9c1ffde72a67353087982da
Navgator_yin <1020351371@qq.com>
2020-09-19 19:49:27 +0800

    新建java代码 helloworld

---
+++ helloworld/src/helloworld/hello.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class hello {
+	int j=5;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a=3.14f;
+		System.out.println(a);
+		
+	}
+
+}
